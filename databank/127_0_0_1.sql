121f0c50c949a5c1b5da68ccbf91038a7734962a
Colossus90 <jens.mollen@gmail.com>
2016-05-22 20:12:09 +0200

    Databank

---
+++ databank/127_0_0_1.sql
@@ -0,0 +1,101 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 22, 2016 at 06:11 PM
+-- Server version: 5.7.9
+-- PHP Version: 5.6.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diw`
+--
+CREATE DATABASE IF NOT EXISTS `diw` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `diw`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_createpost_id` int(11) NOT NULL,
+  `fk_users_id` int(11) NOT NULL,
+  `fk_comment_id` int(11) NOT NULL,
+  `comment` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `createpost`
+--
+
+DROP TABLE IF EXISTS `createpost`;
+CREATE TABLE IF NOT EXISTS `createpost` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` timestamp NOT NULL,
+  `titel` varchar(13) NOT NULL,
+  `categorie` varchar(20) NOT NULL,
+  `inleiding` varchar(300) NOT NULL,
+  `paragraaf` varchar(3000) NOT NULL,
+  `fk_user_id` int(11) NOT NULL,
+  `photo` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `upvote`
+--
+
+DROP TABLE IF EXISTS `upvote`;
+CREATE TABLE IF NOT EXISTS `upvote` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fk_post_id` int(11) NOT NULL,
+  `fk_user_id` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `mail`, `photo`) VALUES
+(1, 'Admin', '5f4dcc3b5aa765d61d8327deb882cf99', 'jens.mollen@gmail.com', '/'),
+(2, 'DIW', '5f4dcc3b5aa765d61d8327deb882cf99', 'contact@vrt.be', '/');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
